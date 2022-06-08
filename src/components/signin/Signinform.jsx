9838b9a2e00f14d9d133f7f9a9884f19a4ff7f62
David Okani <itsdavidokani@gmail.com>
2022-06-08 12:25:26 +0100

    rechecking on the already done signin form

--- src/components/signin/Signinform.jsx
+++ src/components/signin/Signinform.jsx
@@ -12,11 +12,11 @@ const Signinform = () => {
 
                     <div className="logform-option">
                         <label id="logform-option" htmlFor="email/phone">Email or Phone Number</label>
-                        <input type="text" name="email phone" id="email/phone" />
+                        <input type="text" name="email phone" id="email/phone" required />
                     </div>
                     <div className="logform-option">
                         <label id="logform-option" htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
+                        <input type="password" name="password" id="password" required />
                     </div>
                     <div className="logform-option">
                         <button className="btn-logform">LOG IN</button>
